670cc51c71cc6fb4ca365faaa699f620b05f1692
lechub <lechub@tlen.pl>
2021-01-18 14:22:26 +0100

    Start ---

---
+++ src/main.rs
@@ -0,0 +1,82 @@
+
+//use std::fmt;
+
+extern crate clap;
+use clap::{Arg, App, SubCommand};
+
+
+fn test(var1:u32)->u32{
+	var1 + 1
+}
+
+fn main() {
+	println!("Program start");
+
+
+    let matches = App::new("My Super Program")
+                          .version("1.0")
+                          .author("LB")
+                          .about("Does awesome things")
+                          .arg(Arg::with_name("config")
+                               .short("c")
+                               .long("config")
+                               .value_name("FILE")
+                               .help("Sets a custom config file")
+                               .takes_value(true))
+                          .arg(Arg::with_name("INPUT")
+                               .help("Sets the input file to use")
+                               .required(false)
+                               .index(1))
+                          .arg(Arg::with_name("v")
+                               .short("v")
+                               .multiple(true)
+                               .help("Sets the level of verbosity"))
+                          .subcommand(SubCommand::with_name("test")
+                                      .about("controls testing features")
+                                      .version("1.3")
+                                      .author("Someone E. <someone_else@other.com>")
+                                      .arg(Arg::with_name("debug")
+                                          .short("d")
+                                          .help("print debug information verbosely")))
+                          .get_matches();
+
+    // Gets a value for config if supplied by user, or defaults to "default.conf"
+    let config = matches.value_of("config").unwrap_or("default.conf");
+    println!("Value for config: {}", config);
+
+    // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
+    // required we could have used an 'if let' to conditionally get the value)
+	if let Some(input_file) = matches.value_of("INPUT"){
+		println!("Using input file: {}", input_file);
+	}else{
+		println!("No imnput file");
+	}
+    
+
+    // Vary the output based on how many times the user used the "verbose" flag
+    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
+    match matches.occurrences_of("v") {
+        0 => println!("No verbose info"),
+        1 => println!("Some verbose info"),
+        2 => println!("Tons of verbose info"),
+        3 | _ => println!("Don't be crazy"),
+    }
+
+    // You can handle information about subcommands by requesting their matches by name
+    // (as below), requesting just the name used, or both at the same time
+    if let Some(matches) = matches.subcommand_matches("test") {
+        if matches.is_present("debug") {
+            println!("Printing debug info...");
+        } else {
+            println!("Printing normally...");
+        }
+    }
+
+
+	let v1 = Box::new(1u32);
+
+	println!(" {}", test(*v1));	
+	println!(" {}", test(*v1));	
+}
+
+
